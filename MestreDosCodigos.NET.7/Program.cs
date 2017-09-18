f4eeb2351ed5bbcf8484322fa4083cd16a72b116
Diego Campanari <campanari@msn.com>
2017-09-18 20:52:32 -0300

    Término da tarefa 7

---
+++ MestreDosCodigos.NET.7/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MestreDosCodigos.NET._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{DateTime.Now} Início");
+
+            Task.WaitAll(
+                DownloadAsync(ProfileUrl),
+                DownloadAsync(RepositoriesUrl));
+
+            Console.WriteLine($"{DateTime.Now} Fim");
+
+            Console.ReadKey();
+        }
+
+        private const string ProfileUrl = "https://api.github.com/users/Campanari";
+        private const string RepositoriesUrl = "https://api.github.com/users/Campanari/repos";
+
+        private static async Task<string> DownloadAsync(string url)
+        {
+            await Task.Delay(new Random().Next(3000));
+
+            Console.WriteLine($"{DateTime.Now} Início - {url}");
+
+            var client = new HttpClient();
+
+            var response = await client.GetAsync(url);
+
+            var @return = await response.Content.ReadAsStringAsync();
+
+            Console.WriteLine($"{DateTime.Now} Fim - {url}");
+
+            return @return;
+        }
+    }
+}
