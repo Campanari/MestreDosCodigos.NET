3f29f129a66d82dffb7519a341bce1630defaef0
Diego Campanari <campanari@msn.com>
2017-09-17 22:37:27 -0300

    ConcluÃÃo da tarefa 5

---
+++ MestreDosCodigos.NET.5/IndexedList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MestreDosCodigos.NET._5
+{
+    public class IndexedList : IEnumerable<string>
+    {
+        private IDictionary<int, string> _values = new Dictionary<int, string>
+        {
+            [1] = "Valor1",
+            [2] = "Valor2",
+            [3] = "Valor3"
+        };
+
+        public string this[int id] =>
+            _values[id];
+
+        public IEnumerator<string> GetEnumerator() =>
+            _values.Values.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() =>
+            GetEnumerator();
+    }
+}
---
+++ MestreDosCodigos.NET.5/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MestreDosCodigos.NET._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inicializar enumerable");
+
+            var generateEnumerable = GenerateEnumerable(7);
+
+            Console.WriteLine("Inicializar lista");
+            var generateListed = GenerateListed(7);
+
+            Console.WriteLine("Enumerar enumerable");
+            generateEnumerable.Count();
+
+            Console.ReadKey();
+        }
+
+        static IEnumerable<int> GenerateEnumerable(int lenght)
+        {
+            var random = new Random();
+
+            for (var i = 0; i < lenght; ++i)
+            {
+                var temp = random.Next();
+
+                Console.WriteLine(temp);
+
+                yield return temp;
+            }
+        }
+
+        static IEnumerable<int> GenerateListed(int lenght)
+        {
+            var random = new Random();
+            var list = new List<int>();
+
+            for (var i = 0; i < lenght; ++i)
+            {
+                var temp = random.Next();
+
+                Console.WriteLine(temp);
+
+                list.Add(temp);
+            }
+
+            return list;
+        }
+    }
+}
