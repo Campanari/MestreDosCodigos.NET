734a3bc564270b4752ab3187b507aaf18a4b1b69
Diego Campanari <campanari@msn.com>
2017-09-18 22:59:30 -0300

    Término da tarefa 9

---
+++ MestreDosCodigos.NET.9/ClasseEstatica.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace MestreDosCodigos.NET._9
+{
+    public static class ClasseEstatica
+    {
+        static ClasseEstatica()
+        {
+            Valores = new[]
+            {
+                2m,
+                3.7m,
+                9.3m
+            };
+        }
+
+        public static IEnumerable<decimal> Valores { get; }
+
+        public static decimal Calclular(decimal valorA, decimal valorB) =>
+            valorA + valorB;
+    }
+}
---
+++ MestreDosCodigos.NET.9/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MestreDosCodigos.NET._9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var valor in ClasseEstatica.Valores)
+                Console.WriteLine(valor);
+
+            var valorCalculado = ClasseEstatica.Calclular(4m, 3.5m);
+
+            Console.WriteLine(valorCalculado);
+
+            Console.ReadKey();
+        }
+    }
+}
